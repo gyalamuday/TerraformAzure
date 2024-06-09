10d62ec124dc17f0c3d311cbac9eab9bf398c4d3
gyalamuday <udaykumargyalam@gmail.com>
2024-06-08 22:44:33 -0500

    removed unwatned files

--- main.tf
+++ main.tf
@@ -28,19 +28,33 @@ provider "azurerm" {
    private_ip_address_allocation = "Dynamic"
  }
 } 
- /* resource "azurerm_virtual_machine" "TRVMSet" {
+resource "azurerm_virtual_machine" "TRVMSet" {
   name = "VMtertest"
   resource_group_name = azurerm_resource_group.rg.name
   location = "EastUS"
-  vm_size = "Standard_DS1_v2"
+  vm_size = "Standard_B1s"
   network_interface_ids = [azurerm_network_interface.nic.id]
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
   storage_os_disk {
    name = "tervmdisk"
    create_option = "FromImage"
    caching = "ReadWrite"
    os_type = "Linux"
- }
-} */
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
 /*  data "azurerm_virtual_machine" "impvs" {
    resource_group_name = azurerm_resource_group.rg.name
    name = "TestTer"
